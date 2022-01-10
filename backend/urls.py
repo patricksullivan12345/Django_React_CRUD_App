77947d26091fda3d3a4aeaf37b2557877f3cab87
patricksullivan12345 <77989210+patricksullivan12345@users.noreply.github.com>
2022-01-10 06:42:09 -0700

    Delete backend directory

--- backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path('post-list/',views.taskList, name="Task list"),
-    path('post-detail/<str:pk>/',views.taskDetail, name="Task detail"),
-    path('post-published/',views.taskQuery, name="Task Query"),
-    path('helloworld/',views.helloworld, name="Hello world")
-]
